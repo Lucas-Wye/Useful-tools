a88b535b8ec0a7c23179376a4492a31240ce3f46
Lucas <lucas.zw.ye@outlook.com>
2020-10-26 11:21:57 +0800

    add keccak algorithm src

--- Crypto/rc4.c
+++ Crypto/rc4.c
@@ -4,11 +4,17 @@
   function: encrytion and decryption with rc4
 */
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAX 256
-#define swap(a,b) {short t;t = a;a = b;b = a;}
+#define swap(a, b) \
+  {                \
+    short t;       \
+    t = a;         \
+    a = b;         \
+    b = a;         \
+  }
 
 void rc4_init(unsigned char *S, unsigned char *key, unsigned long key_len)
 {
@@ -17,17 +23,17 @@ void rc4_init(unsigned char *S, unsigned char *key, unsigned long key_len)
   unsigned char T[MAX] = {0};
 
   //(1)Initialization
-  for(i = 0; i < MAX; i++)
+  for (i = 0; i < MAX; i++)
   {
     S[i] = i;
     T[i] = key[i % key_len];
   }
 
   //(2)Permutation of S
-  for(i = 0; i < MAX; i++)
+  for (i = 0; i < MAX; i++)
   {
     j = (j + S[i] + T[i]) % MAX;
-    swap(S[i],S[j]);
+    swap(S[i], S[j]);
   }
 }
 
@@ -37,17 +43,17 @@ void rc4_crypto(unsigned char *S, unsigned char *data, unsigned long data_len)
   short i = 0;
   short j = 0;
   unsigned long k = 0;
-  for(k = 0; k < data_len; k++)
+  for (k = 0; k < data_len; k++)
   {
     i = (i + 1) % MAX;
     j = (j + S[i]) % MAX;
-    swap(S[i],S[j]);
+    swap(S[i], S[j]);
     data[k] ^= S[(S[i] + S[j]) % MAX];
   }
 }
 
-
-int main(void){
+int main(void)
+{
   unsigned char s1[MAX] = {0};
   unsigned char s2[MAX] = {0};
   char key[MAX] = {0};
@@ -63,7 +69,7 @@ int main(void){
   rc4_init(s1, (unsigned char *)key, strlen(key));
 
   // backup for S box
-  for(short i = 0; i < MAX; i++)
+  for (short i = 0; i < MAX; i++)
   {
     s2[i] = s1[i];
   }
@@ -80,5 +86,4 @@ int main(void){
   printf("Decryption result: %s\n", data);
 
   return 0;
-  
 }
