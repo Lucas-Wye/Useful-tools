f6fc540fc937f56a9be78801c095d6d81c5d30f5
Lucas <lucas.zw.ye@outlook.com>
2021-01-29 11:51:57 +0800

    add nr, nn, rn

--- Crypto/ntt/ntt.py
+++ Crypto/ntt/ntt.py
@@ -50,7 +50,7 @@ def DIT_CT_Butterfly(A0, A1, W, q):
 
 
 # Bit-Reverse integer
-def intReverse(a,n):
+def intReverse(a, n):
     b = ('{:0'+str(n)+'b}').format(a)
     return int(b[::-1],2)
 
@@ -64,54 +64,204 @@ def Radix2_DIT_Iterative_NTT_NR(A,W,q):
     N = len(A)
     B = [_ for _ in A]
 
+    cindex_1 = [[] for _ in range(int(log(N, 2)))]
+    cindex_2 = [[] for _ in range(int(log(N, 2)))]
+    cnt = 0
+
+    BF_count = 0 
     for s in range(int(log(N,2)),0,-1):
         m = 2**s
         for k in range(int(N/m)):
-            TW = pow(W,intReverse(k,int(log(N,2))-s)*int(m/2),q)
-            for j in range(int(m/2)):
-                u = B[k*m+j]
-                t = (TW*B[k*m+j+int(m/2)]) % q
-                
-                B[k*m+j]          = (u+t) % q
-                B[k*m+j+int(m/2)] = (u-t) % q
-
-    return B
-
-def my_Radix2_DIT_Iterative_NTT_NR(A,W,q):
-    N = len(A)
-    B = [_ for _ in A]
-
-    index = [[] for _ in range(int(log(N, 2)))]
-
-    for s in range(int(log(N,2)),0,-1):
-        m = 2**s
-        for k in range(int(N/m)):
-            TW = pow(W,intReverse(k,int(log(N,2))-s)*int(m/2),q)
+            TW = pow(W,\
+                    intReverse(k,int(log(N,2))-s)*int(m/2),\
+                    q)
             for j in range(int(m/2)):
                 index_1 = k*m +j
                 index_2 = k*m +j +int(m/2)
-
                 B[index_1], B[index_2] = DIT_CT_Butterfly(B[index_1],B[index_2],TW, q) 
-                index[s-1].append(str(index_1)+' '+str(index_2)) 
+                BF_count += 1
 
+                cindex_1[cnt].append(index_1)
+                cindex_2[cnt].append(index_2)
+        cnt += 1
+
+            
+    # bit reverse
+    tmp_index_1 = []
+    tmp_index_2 = []
+    for i in range(len(cindex_1[-1])):
+        tmp_1 = cindex_1[-1][i]
+        tmp_2 = cindex_2[-1][i]
+        tmp_1 = intReverse(tmp_1, int(log(N,2))) 
+        tmp_2 = intReverse(tmp_2, int(log(N,2))) 
+        tmp_index_1.append(tmp_1)
+        tmp_index_2.append(tmp_2)
+    cindex_1.append(tmp_index_1)
+    cindex_2.append(tmp_index_2)
+
+    print(BF_count)
+    for i in range(len(cindex_1[0])):
+        for j in range(len(cindex_1)):
+            print(cindex_1[j][i], cindex_2[j][i],end="\t")
+        print()
+
+    return B
+
+
+# Iterative Radix-2 Decimation-in-Time (DIT) (CT) NTT - RN
+# A: input polynomial (bit-reversed order)
+# W: twiddle factor
+# q: modulus
+# B: output polynomial (standard order)
+def Radix2_DIT_Iterative_NTT_RN(A,W,q): 
+    N = len(A) 
+    B = [_ for _ in A]
+
+    v = int(N/2)
+    m = 1
+    d = 1
+
+    index_1 = [[] for _ in range(int(log(N, 2)))]
+    index_2 = [[] for _ in range(int(log(N, 2)))]
+    cnt = 0
+    BF_count = 0 
+    while m<N:
+        np = 2*m
+        lp = np*(v-1)
+        for k in range(m):
+            TW = pow(W,k*v,q)
+            j = k
+            while j < (k+lp+1):
+                B[j], B[j+d] = DIT_CT_Butterfly(B[j],B[j+d],TW, q) 
+                BF_count += 1
+                index_1[cnt].append(j)
+                index_2[cnt].append(j+d)
+                j = j+np
+        v = int(v/2)
+        m = 2*m
+        d = 2*d
+        cnt += 1
+
+    # bit reverse
+    tmp_index_1 = []
+    tmp_index_2 = []
+    for i in range(len(index_1[0])):
+        tmp_1 = index_1[0][i]
+        tmp_2 = index_2[0][i]
+        tmp_1 = intReverse(tmp_1, int(log(N,2))) 
+        tmp_2 = intReverse(tmp_2, int(log(N,2))) 
+        tmp_index_1.append(tmp_1)
+        tmp_index_2.append(tmp_2)
+    index_1.insert(0, tmp_index_1)
+    index_2.insert(0, tmp_index_2)
+    print(BF_count)
+    for i in range(len(index_1[0])):
+        for j in range(len(index_1)):
+            print(index_1[j][i], index_2[j][i],end="\t")
+        print()
+
+    return B
+
+
+
+# Iterative Radix-2 Decimation-in-Time (DIT) (CT) NTT - NN
+# A: input polynomial (standard order)
+# W: twiddle factor
+# q: modulus
+# B: output polynomial (standard order)
+def Radix2_DIT_Iterative_NTT_NN(A,W,q):
+    N = len(A)
+    B = [_ for _ in A]
+    C = [_ for _ in A]
+    # C = [0]*N
+
+    v = int(N/2)
+    m = 1
+    d = int(N/2)
+
+    if int(log(v))%2 == 0:
+        nsi = True
+    else:
+        nsi = False
+
+    index_1 = [[] for _ in range(int(log(N, 2)))]
+    index_2 = [[] for _ in range(int(log(N, 2)))]
+    cnt = 0
+    BF_count = 0
+
+    while m<N:
+        l = 0
+        for k in range(m):
+            jf = 2*k*v
+            jl = jf + v - 1
+            jt = k*v
+
+            TW = pow(W,jt,q)
+
+            for j in range(jf,jl+1):
+                if nsi:
+                    C[l], C[l+int(N/2)] = DIT_CT_Butterfly(B[j],B[j+d],TW, q) 
+                else:
+                    B[l], B[l+int(N/2)] = DIT_CT_Butterfly(C[j],C[j+d],TW, q) 
+                BF_count += 1
+                index_1[cnt].append(j)
+                index_2[cnt].append(j+d)
+
+                l = l+1
+        nsi = (not nsi) 
+        cnt += 1
+        v = int(v/2)
+        m = 2*m
+        d = int(d/2)
+
+    print(BF_count)
+    for i in range(len(index_1[0])):
+        for j in range(len(index_1)):
+            print(index_1[j][i], index_2[j][i],end="\t")
+        print()
+    return C
+
+def another_DIT(n):
+    # https://zjueducn-my.sharepoint.com/personal/lucas_zw_ye_zju_edu_cn/_layouts/OneNote.aspx?id=%2Fpersonal%2Flucas_zw_ye_zju_edu_cn%2FDocuments%2FNotebooks%2FMy%20Notebook%20%40%20zju.edu.cn&wd=target%28PQC.one%7CCC5317AE-695E-4D9C-BD2C-8B2061C79D76%2FNTT%7C185E72CC-68F7-4A21-B61E-B9EA439524F3%2F%29 onenote:https://zjueducn-my.sharepoint.com/personal/lucas_zw_ye_zju_edu_cn/Documents/Notebooks/My%20Notebook%20@%20zju.edu.cn/PQC.one#NTT&section-id={CC5317AE-695E-4D9C-BD2C-8B2061C79D76}&page-id={185E72CC-68F7-4A21-B61E-B9EA439524F3}&object-id={6888E231-8200-0EE5-2E9F-87107CBAED78}&45
+    a = [i for i in range(n)]
+    #for i in range(n):
+    #    a[i]=intReverse(i, int(log(n,2)))
+    #    print('a[', i ,'] =',a[i])
+    index = [[] for _ in range(int(log(n,2)))]
+    cnt = 0 
+    m = 2 
+    while m <= n:
+        j = 0
+        for j in range(int(m/2)):
+            k = 0
+            while k <= n-1:
+                index[cnt].append(str(a[k+j])+' ' + str(a[k+j+int(m/2)]))
+                k = k + m
+        m = 2 * m
+        cnt = cnt + 1
     for i in range(len(index[0])):
         for j in range(len(index)):
             print(index[j][i],end="\t")
         print()
-            
-    return B
 
-def test_ntt_nr():
+
+def test_ntt(point):
     import random
     for _ in range(1):
         q_bit = random.randint(10, 32)
         q = generate_large_prime(q_bit)
-        A = [random.randint(1,q) for _ in range(16)]
+        A = [random.randint(1,q) for _ in range(point)]
         B = random.randint(1,q)
+        print('NR')
         r1 = Radix2_DIT_Iterative_NTT_NR(A, B, q)
-        r2 = my_Radix2_DIT_Iterative_NTT_NR(A,B,q)
-        for i in range(len(r1)): 
-            if r1[i] != r2[i]: 
-                print(i, r1[i], r2[i], " fail")
-                break
-test_ntt_nr()        
+        print('\nRN')
+        r2 = Radix2_DIT_Iterative_NTT_RN(A, B, q)
+        print('\nNN')
+        r3 = Radix2_DIT_Iterative_NTT_NN(A, B, q)
+        print('\nanother')
+        another_DIT(16)
+        #for i in range(len(r1)):
+        #    if r1[i] != r2[i]:
+        #        print('fail')
+        #        break
+test_ntt(16)        
